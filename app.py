563b442df0b6a98fbf9c9325b31bd8f986fc7f99
𝐀𝐑𝐒𝐇`𝐗´ 😈 <149147034+asrshx@users.noreply.github.com>
2026-05-21 03:18:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1510,4 +1510,4 @@ if __name__ == '__main__':
     ║     Offline Automation System        ║
     ╚══════════════════════════════════════╝
     """)
-    app.run(host='0.0.0.0', port=5000, debug=False, threaded=True)
+    app.run(host='0.0.0.0', port=8080, debug=False, threaded=True)
